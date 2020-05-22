1ef114ffea189f3e35fe2e2ec1b07f399cc4ff20
Adil Gondal <adil.gondal@yale.edu>
2020-05-22 07:05:02 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,10 +2,10 @@ def find_max_value(array)
   count = 0
   max = array[0]
   while count < array.length do
-    if max_value < array[count]
-      max_value = array[count]
+    if max < array[count]
+      max = array[count]
     end
     count += 1
   end
-  max_value
+  max
 end
